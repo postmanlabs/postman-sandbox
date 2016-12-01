6c4ecd9c2a021bdb61e2ee82a50d064b4c6ad9d7
Shamasis Bhattacharya <mail@shamasis.net>
2016-12-01 22:01:12 +0530

    Added test for loads library inside sandbox

--- test/unit/sandbox-sanity.test.js
+++ test/unit/sandbox-sanity.test.js
@@ -4,11 +4,7 @@ describe('sandbox', function () {
 
     it('must create context', function (done) {
         Sandbox.createContext(function (err, ctx) {
-
-            if (err) {
-                return done(err);
-            }
-
+            if (err) { return done(err); }
             ctx.on('error', done);
 
             ctx.ping(function (err, ttl, packet) {
@@ -21,20 +17,31 @@ describe('sandbox', function () {
         });
     });
 
-    it.skip('must execute a piece of code', function (done) {
+    it('must execute a piece of code', function (done) {
         Sandbox.createContext(function (err, ctx) {
-
-            if (err) {
-                return done(err);
-            }
-
+            if (err) { return done(err); }
             ctx.on('error', done);
 
-            ctx.ping(function (err, packet) {
-                expect(err).to.not.be.ok();
-                expect(packet).to.be.ok();
+            ctx.execute('throw new Error("this will regurgitate!")', function (err) {
+                expect(err).be.ok();
+                expect(err).have.property('message', 'this will regurgitate!');
                 done();
             });
+        });
+    });
+
+    it('must have (and not have) a few important globals', function (done) {
+        Sandbox.createContext(function (err, ctx) {
+            if (err) { return done(err); }
+            ctx.on('error', done);
+
+            ctx.execute(`
+                var assert = require('assert');
+                assert.equal(typeof _, 'function');
+                assert.equal(typeof window, 'undefined');
+                assert.equal(typeof Error, 'function');
+                assert.equal(typeof console, 'object');
+            `, done);
         });
     });
 });
