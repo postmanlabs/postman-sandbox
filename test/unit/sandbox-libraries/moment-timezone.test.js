0a9838e56b2e23bbde30a235a5b5c945b2924b1d
Kunal Nagpal <kunagpal@gmail.com>
2016-12-30 18:53:32 +0530

    Completed pending tests

--- test/unit/sandbox-libraries/moment-timezone.test.js
+++ test/unit/sandbox-libraries/moment-timezone.test.js
@@ -1,4 +1,4 @@
-describe.only('sandbox library - moment-timezone', function () {
+describe('sandbox library - moment-timezone', function () {
     this.timeout(1000 * 60);
     var Sandbox = require('../../../'),
         context;
@@ -26,18 +26,106 @@ describe.only('sandbox library - moment-timezone', function () {
         context.execute(`
             var assert = require('assert');
 
-            assert.strictEqual(moment(1483245610000).format('MMMM Do YYYY, h:mm:ss a'),
+            assert.strictEqual(moment('2017-01-01T10:10:10.000').format('MMMM Do YYYY, h:mm:ss a'),
                 'January 1st 2017, 10:10:10 am');
-            assert.strictEqual(moment(1483245610000).format('dddd'), 'Sunday');
-            assert.strictEqual(moment(1483245610000).format("MMM Do YY"), 'Jan 1st 17');
-            assert.strictEqual(moment(1483245610000).format('YYYY [escaped] YYYY'), '2017 escaped 2017');
+            assert.strictEqual(moment('2017-01-01T10:10:10.000').format('dddd'), 'Sunday');
+            assert.strictEqual(moment('2017-01-01T10:10:10.000').format("MMM Do YY"), 'Jan 1st 17');
+            assert.strictEqual(moment('2017-01-01T10:10:10.000').format('YYYY [escaped] YYYY'), '2017 escaped 2017');
         `, done);
     });
 
-    it.skip('must format relative time', function () {});
-    it.skip('must format calendar time', function () {});
+    it('must format relative time', function (done) {
+        context.execute(`
+            var assert = require('assert'),
+                start = moment([2007, 0, 28]),
+                end = moment([2007, 0, 29]);
 
-    describe.skip('locales', function () {});
+            assert.strictEqual(start.to(start, true), 'a few seconds');
+            assert.strictEqual(start.to(start), 'a few seconds ago');            
+
+            assert.strictEqual(start.to(end, true), 'a day');
+            assert.strictEqual(start.to(end), 'in a day');
+
+            assert.strictEqual(end.to(start, true), 'a day');
+            assert.strictEqual(end.to(start), 'a day ago');
+        `, done);
+    });
+
+    it('must format calendar time', function (done) {
+        context.execute(`
+            var assert = require('assert');
+
+            assert.strictEqual(moment('2017-01-01T10:10:10.000').calendar(), 'Sunday at 10:10 AM');
+
+            assert.strictEqual(moment('2017-01-01T10:10:10.000').subtract(1, 'day').calendar(), 'Tomorrow at 10:10 AM');
+            assert.strictEqual(moment('2017-01-01T10:10:10.000').subtract(10, 'days').calendar(), '12/22/2016');
+
+            assert.strictEqual(moment('2017-01-01T10:10:10.000').add(1, 'day').calendar(), 'Monday at 10:10 AM');
+            assert.strictEqual(moment('2017-01-01T10:10:10.000').add(10, 'days').calendar(), '01/11/2017');
+        `, done);
+    });
+
+    describe('locales', function () {
+        it('must work with the US locale', function (done) {
+            context.execute(`
+                var assert = require('assert');
+
+                moment.locale('en');
+                assert.strictEqual(moment.locale(), 'en');
+
+                assert.strictEqual(moment.weekdays(3), 'Wednesday');
+                assert.strictEqual(moment.weekdaysShort(3), 'Wed');
+
+                assert.strictEqual(moment.months(1), 'February');
+                assert.strictEqual(moment.monthsShort(1), 'Feb');
+            `, done);
+        });
+
+        it('must work with the UK locale', function (done) {
+            context.execute(`
+                var assert = require('assert');
+
+                moment.locale('en-gb');
+                assert(moment.locale(), 'en-gb');
+
+                assert(moment.weekdays(3), 'Wednesday');
+                assert(moment.weekdaysShort(3), 'Wed');
+
+                assert(moment.months(1), 'February');
+                assert(moment.monthsShort(1), 'Feb');
+            `, done);
+        });
+
+        it('must work with the Chinese locale', function (done) {
+            context.execute(`
+                var assert = require('assert');
+
+                moment.locale('zh-cn');
+                assert(moment.locale(), 'zh-cn');
+
+                assert(moment.weekdays(3), '星期三');
+                assert(moment.weekdaysShort(3), '周三');
+
+                assert(moment.months(1), '二月');
+                assert(moment.monthsShort(1), '2月');
+            `, done);
+        });
+
+        it('must work with the pseudo-locale', function (done) {
+            context.execute(`
+                var assert = require('assert');
+
+                moment.locale('x-pseudo');
+                assert(moment.locale(), 'x-pseudo');
+
+                assert(moment.weekdays(3), 'Wéd~ñésd~áý');
+                assert(moment.weekdaysShort(3), '~Wéd');
+
+                assert(moment.months(1), 'F~ébrú~árý');
+                assert(moment.monthsShort(1), '~Féb');
+            `, done);
+        });
+    });
 
     describe('timezone', function () {
         it('must format dates in any timezone', function (done) {
