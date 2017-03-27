998192ffd7b239c77387835e6eec79fe2ab96feb
Shamasis Bhattacharya <mail@shamasis.net>
2017-03-27 15:00:10 +0530

    Added a test in unit test bootstrap to ensure that actual tests don’t waste time in bundling the sandbox

--- test/unit/_bootstrap.js
+++ test/unit/_bootstrap.js
@@ -9,3 +9,17 @@ after(function () {
     _expect ? (global.expect = _expect) : (delete global._expect);
     _expect = null;
 });
+
+describe('_bootstrap', function () {
+    this.timeout(1000 * 60); // set a timeout
+
+    var Sandbox = require('../../lib');
+    it('bundling should work for sandbox', function (done) {
+        // we simply create a context and run to ensure it is working
+        Sandbox.createContext(function (err, ctx) {
+            if (err) { return done(err); }
+            ctx.on('error', done);
+            ctx.ping(done);
+        });
+    });
+});
