337726e3b9449901964415e9b94af2f5d94a88e0
Shamasis Bhattacharya <mail@shamasis.net>
2017-05-23 01:56:28 +0530

    Fixed a bug where synchronously clearing timer after setting was not working as expected

--- test/unit/sandbox-timers.test.js
+++ test/unit/sandbox-timers.test.js
@@ -17,7 +17,7 @@ describe('timers inside sandbox', function () {
     });
 
 
-    it('must work inside sandbox', function (done) {
+    it('must work with setTimeout inside sandbox', function (done) {
         ctx.execute(`
             var startTime = Date.now();
             setTimeout(function () {
@@ -38,7 +38,57 @@ describe('timers inside sandbox', function () {
         });
     });
 
-    it('must time out if timers run beyond interval', function (done) {
+    it('must be able to clear timeout', function (done) {
+        var timeoutExecuted = 'timeout not executed';
+
+        ctx.on('console', function (cursor, level, message) { // keep track of intervals passed
+            if (message === 'timeout') { timeoutExecuted = 'timeout executed'; }
+        });
+
+        ctx.execute(`
+            var id = setTimeout(function () {
+                console.log('timeout');
+            }, 100);
+            clearTimeout(id);
+        `, {
+            // debug: false,
+            timeout: 200
+        }, function (err, res) {
+            if (err) { return done(err); }
+
+            expect(err).to.not.be.ok();
+            expect(res.return.async).to.be(false);
+
+            // we wait for a while to ensure that the timeout was actually cleared.
+            setTimeout(function () {
+                expect(timeoutExecuted).to.eql('timeout not executed');
+                done();
+            }, 150);
+        });
+    });
+
+    it('must work with setImmediate inside sandbox', function (done) {
+        ctx.execute(`
+            var startTime = Date.now();
+            setImmediate(function () {
+                pm.globals.set('executed', 1, 'boolean');
+            });
+        `, {
+            timeout: 200
+        }, function (err, res) {
+            if (err) { return done(err); }
+
+            expect(err).to.not.be.ok();
+            expect(res.return.async).to.be(true);
+
+            expect(res).to.have.property('globals');
+            expect(res.globals.values).to.be.an('array');
+            expect(res.globals.values[0].value).to.be(true);
+            done();
+        });
+    });
+
+    it('must time out if timers run beyond interval and stop the interval', function (done) {
         var count = {
             terminal: null,
             current: 0
