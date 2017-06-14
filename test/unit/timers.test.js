fc044001be6e1d54ad5d0ed6b4e151349ad2acb3
Shamasis Bhattacharya <mail@shamasis.net>
2017-06-14 16:55:23 +0530

    Added poor shenanigans around sandbox timers to handle setEvent edge cases (needs a redo later)

---
+++ test/unit/timers.test.js
@@ -0,0 +1,22 @@
+describe('timers module', function () {
+    var Timers = require('../../lib/sandbox/timers');
+
+    it('must be able to set an event', function (done) {
+        var status = 'uneventful',
+            timers,
+            id;
+
+        timers = new Timers(null, done, null, function () {
+            expect(status).to.eql('eventful');
+            done();
+        });
+
+        id = timers.setEvent(function (msg) {
+            status = msg;
+        });
+
+        setTimeout(function () {
+            timers.clearEvent(id, 'eventful');
+        }, 10);
+    });
+});
