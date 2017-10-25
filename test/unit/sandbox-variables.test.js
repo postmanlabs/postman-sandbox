a9f9b55de1d460d5b9e1c50184a6de2bbd31edf5
Harendra Singh <66hari@gmail.com>
2017-10-25 17:49:39 +0530

    Update tests

--- test/unit/sandbox-variables.test.js
+++ test/unit/sandbox-variables.test.js
@@ -1,15 +1,19 @@
 var sdk = require('postman-collection'),
 
     Sandbox = require('../../lib'),
-    Execution = require('../../lib/sandbox/execution'),
 
-    execution, // eslint-disable-line no-unused-vars
-    ctx;
+    globalVarList = new sdk.VariableList(null, {key: 'key-1', value: 'value-1'}),
+    collectionVarList = new sdk.VariableList(null, {key: 'key-2', value: 'value-2'}),
+    envVarList = new sdk.VariableList(null, {key: 'key-3', value: 'value-3'}),
+    contextData = {'key-4': 'value-4'},
+    localVarList = new sdk.VariableList(null, {key: 'key-5', value: 'value-5'}),
+    ctx,
+    executionResults;
 
 describe('pm.variables', function () {
     this.timeout(1000 * 60);
 
-    beforeEach(function (done) {
+    before(function (done) {
         Sandbox.createContext({debug: true}, function (err, context) {
             if (err) { return done(err); }
             ctx = context;
@@ -17,32 +21,117 @@ describe('pm.variables', function () {
         });
     });
 
-    afterEach(function () {
+    after(function () {
         ctx.dispose();
         ctx = null;
     });
 
-    before(function () {
-        execution = new Execution('id', {listen: 'test'}, {}, {});
-    });
+    describe('.set', function () {
+        before(function (done) {
+            ctx.execute(`
+                pm.variables.set("key-1", "modified");
+                pm.variables.set("key-2", "modified");
+                pm.variables.set("key-3", "modified");
+                pm.variables.set("key-4", "modified");
+                pm.variables.set("key-5", "modified");
+                pm.variables.set("key-6", "new");
+            `, {
+                    timeout: 200,
+                    context: {
+                        globals: new sdk.VariableScope(globalVarList),
+                        collectionVariables: new sdk.VariableScope(collectionVarList),
+                        environment: new sdk.VariableScope(envVarList),
+                        data: contextData,
+                        variables: new sdk.VariableScope(localVarList)
+                    }
+                }, function (err, execution) {
+                    if (err) { return done(err); }
 
-    it('must return only the base definition variables in the result', function (done) {
-        var globalVarList = new sdk.VariableList(null, {key: 'key-1', value: 'value-1'}),
-            envVarList = new sdk.VariableList(null, {key: 'key-2', value: 'value-2'});
+                    executionResults = execution;
+                    return done();
+                });
+        });
 
-        ctx.execute('pm.variables.set("key-3", "value-3");', {
-            timeout: 200,
-            context: {
-                globals: new sdk.VariableScope(globalVarList),
-                environment: new sdk.VariableScope(envVarList)
-            }
-        }, function (err, execution) {
-            if (err) { return done(err); }
+        it('must return the modified variables in the result', function () {
+            expect(executionResults.variables).to.have.property('values');
+            expect(executionResults.variables.values).to.eql([
+                {type: 'any', value: 'modified', key: 'key-5'},
+                {type: 'any', value: 'modified', key: 'key-1'},
+                {type: 'any', value: 'modified', key: 'key-2'},
+                {type: 'any', value: 'modified', key: 'key-3'},
+                {type: 'any', value: 'modified', key: 'key-4'},
+                {type: 'any', value: 'new', key: 'key-6'}
+            ]);
+        });
 
-            expect(execution.variables).to.have.property('values');
-            expect(execution.variables.values).to.eql([{type: 'any', value: 'value-3', key: 'key-3'}]);
-            return done();
+        it('must not modify the globals, envrironment, collection and data variables', function () {
+            expect(executionResults.globals).to.have.property('values');
+            expect(executionResults.collectionVariables).to.have.property('values');
+            expect(executionResults.environment).to.have.property('values');
+
+            expect(executionResults.globals.values).to.eql([
+                {type: 'any', value: 'value-1', key: 'key-1'}
+            ]);
+            expect(executionResults.collectionVariables.values).to.eql([
+                {type: 'any', value: 'value-2', key: 'key-2'}
+            ]);
+            expect(executionResults.environment.values).to.eql([
+                {type: 'any', value: 'value-3', key: 'key-3'}
+            ]);
+            expect(executionResults.data).to.eql({'key-4': 'value-4'});
         });
     });
 
+    describe('.get', function () {
+        it('must return appropriate variables', function (done) {
+            ctx.execute(`
+                var assert = require('assert');
+                assert.strictEqual(pm.variables.get('key-1'), 'value-1');
+                assert.strictEqual(pm.variables.get('key-2'), 'value-2');
+                assert.strictEqual(pm.variables.get('key-3'), 'value-3');
+                assert.strictEqual(pm.variables.get('key-4'), 'value-4');
+                assert.strictEqual(pm.variables.get('key-5'), 'value-5');
+                assert.strictEqual(pm.variables.get('key-6'), undefined);
+            `, {
+                    timeout: 200,
+                    context: {
+                        globals: new sdk.VariableScope(globalVarList),
+                        collectionVariables: new sdk.VariableScope(collectionVarList),
+                        environment: new sdk.VariableScope(envVarList),
+                        data: contextData,
+                        variables: new sdk.VariableScope(localVarList)
+                    }
+                }, function (err, execution) {
+                    if (err) { return done(err); }
+
+                    executionResults = execution;
+                    return done();
+                });
+        });
+
+        it('must reinitialize the variables when same sandbox instance is used again', function (done) {
+            ctx.execute(`
+                var assert = require('assert');
+                assert.strictEqual(pm.variables.get('key-1'), undefined);
+                assert.strictEqual(pm.variables.get('key-2'), undefined);
+                assert.strictEqual(pm.variables.get('key-3'), undefined);
+                assert.strictEqual(pm.variables.get('key-4'), undefined);
+                assert.strictEqual(pm.variables.get('key-5'), undefined);
+                assert.strictEqual(pm.variables.get('key-6'), undefined);
+            `, {
+                    timeout: 200,
+                    context: {}
+                }, function (err, execution) {
+                    if (err) { return done(err); }
+
+                    expect(execution.variables.values).to.eql([]);
+                    expect(execution.globals.values).to.eql([]);
+                    expect(execution.collectionVariables.values).to.eql([]);
+                    expect(execution.environment.values).to.eql([]);
+                    expect(execution.data).to.be.eql([]);
+
+                    return done();
+                });
+        });
+    });
 });
