6cbcb90bc85acdca0f28e234b00a824c8155a0d3
kamalakannan <kamalakannan@getpostman.com>
2017-11-08 15:03:59 +0530

    Added assertion error for legacy test assertions

--- test/unit/sandbox-assertion-events.test.js
+++ test/unit/sandbox-assertion-events.test.js
@@ -47,10 +47,14 @@ describe('sandbox assertion events', function () {
                     pm.expect(123).be.a('number');
                 });
 
-                tests['fail4'] = undefined;
+                tests['fail5'] = undefined;
 
                 pm.test("fail3", function () {
                     throw new Error('sample error 2');
+                });
+
+                pm.test("fail4", function () {
+                    pm.expect('a').to.equal('b');
                 });
 
                 tests['pass4'] = true;
@@ -59,34 +63,55 @@ describe('sandbox assertion events', function () {
     });
 
     it('must be indexed across parallel executions', function () {
-        expect(assestions.map(function (test) { return test.index; })).to.eql([0, 1, 2, 3, 0, 1, 2, 3]);
+        expect(assestions.map(function (test) { return test.index; })).to.eql([0, 1, 2, 3, 0, 1, 2, 3, 4]);
     });
 
     it('must be called for async and sync assertions', function () {
-        expect(assestions.length).to.be(8);
+        expect(assestions.length).to.be(9);
 
         // async tests assertions for 1st execution in order
         expect(assestions[0]).to.have.property('name', 'pass1');
         expect(assestions[0]).to.have.property('passed', true);
+        expect(assestions[0]).to.have.property('error', null);
         expect(assestions[1]).to.have.property('name', 'fail1');
         expect(assestions[1]).to.have.property('passed', false);
+        expect(assestions[1]).to.have.property('error');
+        expect(assestions[1].error).to.have.property('name', 'Error');
+        expect(assestions[1].error).to.have.property('message', 'sample error 1');
 
         // sync tests assestions for 1st execution in order
         expect(assestions[2]).to.have.property('name', 'fail2');
         expect(assestions[2]).to.have.property('passed', false);
+        expect(assestions[2]).to.have.property('error');
+        expect(assestions[2].error).to.have.property('name', 'AssertionError');
+        expect(assestions[2].error).to.have.property('message', 'fail2');
         expect(assestions[3]).to.have.property('name', 'pass2');
         expect(assestions[3]).to.have.property('passed', true);
+        expect(assestions[3]).to.have.property('error', null);
 
         // async tests assertions for 2nd execution in order
         expect(assestions[4]).to.have.property('name', 'pass3');
         expect(assestions[4]).to.have.property('passed', true);
+        expect(assestions[4]).to.have.property('error', null);
         expect(assestions[5]).to.have.property('name', 'fail3');
         expect(assestions[5]).to.have.property('passed', false);
-
-        // sync tests assestions for 2nd execution in order
+        expect(assestions[5]).to.have.property('error');
+        expect(assestions[5].error).to.have.property('name', 'Error');
+        expect(assestions[5].error).to.have.property('message', 'sample error 2');
         expect(assestions[6]).to.have.property('name', 'fail4');
         expect(assestions[6]).to.have.property('passed', false);
-        expect(assestions[7]).to.have.property('name', 'pass4');
-        expect(assestions[7]).to.have.property('passed', true);
+        expect(assestions[6]).to.have.property('error');
+        expect(assestions[6].error).to.have.property('name', 'AssertionError');
+        expect(assestions[6].error).to.have.property('message', 'expected \'a\' to equal \'b\'');
+
+        // sync tests assestions for 2nd execution in order
+        expect(assestions[7]).to.have.property('name', 'fail5');
+        expect(assestions[7]).to.have.property('passed', false);
+        expect(assestions[7]).to.have.property('error');
+        expect(assestions[7].error).to.have.property('name', 'AssertionError');
+        expect(assestions[7].error).to.have.property('message', 'fail5');
+        expect(assestions[8]).to.have.property('name', 'pass4');
+        expect(assestions[8]).to.have.property('passed', true);
+        expect(assestions[8]).to.have.property('error', null);
     });
 });
