98af29a623164ae6af8965df7c08bc919ee96ddc
Udit Vasu <udit@pm.me>
2020-10-04 09:54:00 +0530

    Move postinstall script to cache generation

--- npm/cache.js
+++ npm/cache.js
@@ -40,7 +40,14 @@ module.exports = function (exit) {
 
     console.info(chalk.yellow.bold('Generating bootcode in ".cache" directory...'));
 
-    var options = require('../lib/environment');
+    // First, override locales exported by faker otherwise all the locales data will be
+    // bundled by browserify.
+    fs.writeFileSync(require.resolve('faker/lib/locales'),
+        // only export `en` locale as required by postman-collection
+        // refer: https://github.com/postmanlabs/postman-collection/blob/v3.6.7/lib/superstring/dynamic-variables.js#L1
+        "exports['en'] = require('./locales/en');"); // eslint-disable-line quotes
+
+    const options = require('../lib/environment');
 
     async.parallel([
         async.apply(createBundle, _.merge({
