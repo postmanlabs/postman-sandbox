d359e98d4fdfb81e3bc76b685bd030fd50f61ea8
appurva21 <appurva21@gmail.com>
2022-06-23 22:54:17 +0530

    Add support for sandbox fleet and custom launch template.

---
+++ test/unit/sandbox-fleet.test.js
@@ -0,0 +1,33 @@
+describe('sandbox fleet', function (done) {
+    it('should return the correct context for the given template name', function (done) {
+        //
+    });
+
+    it('should return the context from cache if already initialized', function (done) {
+        //
+    });
+
+    it('should throw for invalid arguments', function (done) {
+        //
+    });
+
+    it('should throw for unregistered template name', function (done) {
+        //
+    });
+
+    it('should execute code using specific template', function (done) {
+        //
+    });
+
+    it('should disable legacy pm APIs via initOptions', function (done) {
+        //
+    });
+
+    it('should disable selected pm APIs provided in initOptions', function (done) {
+        //
+    });
+
+    it('should dispose all contexts', function (done) {
+        //
+    });
+});
