9ce607795e3003be2df88f6426053dc05617e52b
Jonathan Haviv <53003885+jonathanhaviv@users.noreply.github.com>
2025-03-10 15:45:38 -0700

    add pm.message

--- types/index.d.ts
+++ types/index.d.ts
@@ -197,6 +197,15 @@ declare class Postman {
      */
     response: Response;
     /**
+     * Only available in on message sent and received scripts. pm.message contains information about
+     * an individual message (varies by protocol) received from a server or sent from the client
+     * that the script is being run on.
+     *
+     * TODO: define message type
+     * @type {Object}
+     */
+    message: any;
+    /**
      * The cookies object contains a list of cookies that are associated with the domain
      * to which the request was made.
      */
@@ -284,6 +293,7 @@ declare interface Visualizer {
 declare interface Execution {
     request: any;
     response: any;
+    message: any;
     /**
      * Stops the current request and its scripts from executing.
      */
