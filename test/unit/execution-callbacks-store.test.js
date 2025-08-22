a4b43afe0f5ef55dda17161218cf4c9487c3b7a0
Devesh Kumar <devesh.kumar@postman.com>
2025-08-22 21:36:35 +0530

    Fix event id generation from callbacks store regiserter

--- test/unit/execution-callbacks-store.test.js
+++ test/unit/execution-callbacks-store.test.js
@@ -2,12 +2,14 @@ const executionCallbacksStore = require('../../lib/sandbox/utils/callbacks-store
 
 describe('execution-callbacks store', function () {
     it('should be able to set and receive a callback with an event id', function (done) {
-        const eventId = '<sample-event-id>',
-            callback = function () {
+        const callback = function () {
                 //
-            };
+            },
+            eventId = executionCallbacksStore.registerCallback(callback);
 
-        executionCallbacksStore.registerCallback(eventId, callback);
+        expect(typeof eventId).to.be.eql('string');
+
+        expect(eventId.startsWith('event_')).to.be.true;
 
         expect(executionCallbacksStore.getCallback(eventId)).to.be.eql(callback);
 
